715009940f893da86d81f089be2541ee7fa771d6
mostafa69d <mostafa69d@gmail.com>
2018-02-13 16:50:51 -0500

    add basic router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './Home';
+import Players from './Players';
+import Teams from './Teams';
 
 class App extends Component {
   render() {
     return (
+     <BrowserRouter>
       <div>
-        react
+        <NavBar />
+        <Switch>
+          <Route  exact path="/" component={Home} />
+          <Route  path="/teams" component={Teams} />
+          <Route  path="/players" component={Players} />
+          <Route render={({ match }) => (<div>Couldn't find {window.location.href}</div>)} />
+        </Switch>
       </div>
+     </BrowserRouter>
     );
   }
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function(){
+  return(
+    <div className="container navbar">
+      <Link to="/">Home</Link>
+      <nav className="nav-links">
+        <Link to="/teams">Teams</Link>
+        <Link to="/players">Players</Link>
+      </nav>
+    </div>
+  )
+}
