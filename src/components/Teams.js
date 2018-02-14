0d5a45b4fd52bede9487e8382df04a676d070afa
mostafa69d <mostafa69d@gmail.com>
2018-02-14 11:28:48 -0500

    add sidebar for teams

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { getTeamNames } from '../api';
+import Sidebar from './Sidebar';
 
 export default class Teams extends Component{
+  state = {
+    teamNames: [],
+    loading: true
+  }
+
+  componentDidMount(){
+    getTeamNames()
+      .then((teamNames) => {
+        this.setState({ loading:false, teamNames})
+      });
+  }
   render(){
+    const { teamNames, loading } = this.state;
+    const { location, match } = this.props;
     return(
       <div className="container two-columns">
-        Teams
+        <Sidebar
+          title='Teams'
+          list={teamNames}
+          {...this.props}
+          loading={loading}
+        />
+
+        {loading === false && location.pathname === '/teams' 
+          ? <div className="sidebar-instruction">Select a team</div>
+          : null
+        }
       </div>
     );
   }
