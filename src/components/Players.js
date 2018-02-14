988494247a6903dfc2009edb45ebe9483e2288e9
mostafa69d <mostafa69d@gmail.com>
2018-02-14 10:51:45 -0500

    add players

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import { getPlayers } from '../api';
+import Sidebar from './Sidebar';
+import { parse } from 'query-string';
 
 export default class Players extends Component{
+  state = {
+    players: [],
+    loading: true
+  }
+
+  componentDidMount(){
+    const { location } = this.props;
+    location.search ? this.fetchPlayers(parse(location.search).teamId)
+                    : this.fetchPlayers();
+  }
+
+  fetchPlayers(team){
+    getPlayers(team)
+      .then((players) => {
+        this.setState({
+          players,
+          loading: false
+        })
+      })
+  }
   render(){
+    const { loading, players } = this.state;
+    const { location, match } = this.props;
     return(
       <div className="container two-column">
-        Players
+        <Sidebar
+          title='Players'
+          list={players.map((player) => player.name )}
+          loading={loading}
+          {...this.props}
+        />
+        {loading===false && location.pathname==='/players'
+          ? <div className="sidebar-instruction">Select a player</div>
+          : null
+        }
       </div>
     );
   }
